92e20c2317bcebc93b70ed9c37eef4b961998709
Umidjon Amirqulov <uamirqulov5@gmail.com>
2024-04-23 22:23:01 +0500

    create application

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState,useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(null)
+ 
+  return (
+    <>
+   <h1 className='text-3xl text-red-800'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
